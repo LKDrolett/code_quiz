ca24938b86110c3d79d1fc426fa4b945962ed559
LKDrolett <67123915+LKDrolett@users.noreply.github.com>
2020-08-15 23:32:39 -0600

    Add files via upload

---
+++ code_quiz/script.js
@@ -0,0 +1,69 @@
+const startButton = document.getElementById("start-btn")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame)
+
+function startGame()  {
+    console.log("Started")
+    startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    nextQuestion()
+}
+
+function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+     questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        questions: "What is 2 + 2",
+        answers: [
+            { text: "4", correct: true},
+            { text: "85", correct: false},
+            { text: "0", correct:false}
+        ]
+    }
+]
+
+// Declared variables
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Retreives local stroage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Event listener to move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
